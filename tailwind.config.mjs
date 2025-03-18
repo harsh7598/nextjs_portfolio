7b25aa2d053e6736d120132d0f4e712091ff8e0e
harsh7598 <harryht7@gmail.com>
2025-03-18 18:52:19 +0530

    about

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,19 @@
 export default {
     theme: {
       extend: {
+        colors:{
+          lightHover:"#fcf4ff",
+          darkHover:"#2a004a",
+          darkTheme:"#11001F",
+        },
         fontFamily: {
           ovo: ["Ovo", "serif"],
           outfit: ["Outfit", "sans-serif"],
         },
+        boxShadow:{
+          'black' : '4px 4px 0 #000',
+          'white' : '4px 4px 0 #fff',
+        }
       },
     },
     plugins: [],
