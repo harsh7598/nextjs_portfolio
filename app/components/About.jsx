aa4ff30708e957c6f24bf3dace9bfeb249f30947
harsh7598 <harryht7@gmail.com>
2025-03-21 15:26:29 +0530

    contact

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,35 +9,49 @@ const About = () => {
       <h2 className="text-center text-5xl font-Ovo">About me</h2>
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
         <div className="w-64 sm:w-80 rounded-3xl max-w-none">
-            <Image src={assets.user_image} alt="" className="w-full rounded-3xl"/>
+          <Image
+            src={assets.user_image}
+            alt=""
+            className="w-full rounded-3xl"
+          />
         </div>
         <div className="flex-1">
-            <p className="mb-6 max-w-2xl font-Ovo">I am an experienced Frontend Developer with over a 2 years of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
-            <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
-            {infoList.map(({icon, iconDark, title, description},index)=>(
+          <p className="mb-6 max-w-2xl font-Ovo">
+            I am an experienced Frontend Developer with over a 2 years of
+            professional expertise in the field. Throughout my career, I have
+            had the privilege of collaborating with prestigious organizations,
+            contributing to their success and growth.
+          </p>
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
               <li
-  className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#f8e9fd] hover:-translate-y-1 duration-500"
-  style={{
-    transition: "box-shadow 0.5s ease-in-out",
-  }}
-  onMouseEnter={(e) => e.currentTarget.style.boxShadow = "4px 4px 0 #000"}
-  onMouseLeave={(e) => e.currentTarget.style.boxShadow = "none"} key={index}>
-                    <Image src={icon} alt={title} className="w-7 mt-3"/>
-                    <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
-                    <p className="text-gray-600 text-sm">{description}</p>
-                </li>
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#f9effd] hover:-translate-y-1 duration-500"
+                style={{
+                  transition: "box-shadow 0.5s ease-in-out",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.boxShadow = "4px 4px 0 #000")
+                }
+                onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+                key={index}
+              >
+                <Image src={icon} alt={title} className="w-7 mt-3" />
+                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
+                <p className="text-gray-600 text-sm">{description}</p>
+              </li>
             ))}
-            </ul>
-            <h4 className="my-6 text-gray-800 font-Ovo">
-              Tools I use
-            </h4>
-            <ul className="flex items-center gap-3 sm:gap-5">
-              {toolsData.map((tool, index)=>(
-                <li className="flex items-center justify-center w-12 sm:w-14 aspect-square border boder-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500" key={index}>
-                  <Image src={tool} alt="Tool" className="w-5 sm:w-7"/>
-                </li> 
-              ))}
-            </ul>
+          </ul>
+          <h4 className="my-6 text-gray-800 font-Ovo">Tools I use</h4>
+          <ul className="flex items-center gap-3 sm:gap-5">
+            {toolsData.map((tool, index) => (
+              <li
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border boder-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
+                key={index}
+              >
+                <Image src={tool} alt="Tool" className="w-5 sm:w-7" />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
